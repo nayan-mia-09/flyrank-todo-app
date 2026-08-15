a5c8f84c738ee6a7eb8925b1ad22191bf146a2d9
Nayan <dreamerboynayan@gmail.com>
2026-08-16 00:42:01 +0600

    Stage 1: signup and login routes working

--- server.js
+++ server.js
@@ -120,6 +120,42 @@ app.delete("/tasks/:id", async(req, res) => {
 });
 
 
+// Auth Routes Stage 01
+// Sign Up
+
+app.post("/auth/signup", async(req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({error: "Email and Password required."})
+    }
+    const {data,error} = await supabase.auth.signUp({email,password});
+    if(error){
+        return res.status(400).json({error: error.message});
+    }
+    return res.status(201).json(data.user);
+});
+
+// Log in
+app.post("/auth/login", async(req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({error: "Email and Password required."})
+    }
+
+    const {data,error} = await supabase.auth.signInWithPassword({email,password});
+
+    if(error){
+        return res.status(401).json({error: "Invalid login credentials"});
+    }
+
+    return res.status(200).json({
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token
+    });
+});
+
 initDB().then(()=> console.log('Postgres initialized')).catch((err)=> console.error('DB Error:', err));
 app.listen(port,()=>{
     console.log(`Server running on Port: ${port}`)
